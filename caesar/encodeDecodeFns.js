cd449272518a16c2c7a0ddff213393075bf3bfe8
IrinaKorbut <irisharomka96@gmail.com>
2021-05-04 21:59:29 +0200

    fix: fix function

--- caesar/encodeDecodeFns.js
+++ caesar/encodeDecodeFns.js
@@ -1,44 +1,45 @@
 function encode(number, string) {
-    return encodeDecode(number, string)
+    return encodeDecode(number, string);
 }
 
 function decode(number, string) {
-    return encodeDecode( -number, string)
+    return encodeDecode( -number, string);
 }
 
 function encodeDecode(number, string) {
-    const arrFromString = string.split('')
+    const shift = number % 26;
+    const arrFromString = string.split('');
     const newArr = arrFromString.map((char, index) => {
         const currentCode = string.charCodeAt(index);    
-        const futureCode = getFutureCode(currentCode, number);
-        return String.fromCharCode(futureCode)
+        const futureCode = getFutureCode(currentCode, shift);
+        return String.fromCharCode(futureCode);
     })
-    return newArr.join('')
+    return newArr.join('');
 }
 
 function getFutureCode(currentCode, number) {
     let futureCode = currentCode + number;
     if (currentCode >= 65 && currentCode <= 90) {
         if (futureCode > 90) {
-            futureCode = futureCode - 90 + 65 - 1
+            futureCode = futureCode - 90 + 65 - 1;
         }
         if (futureCode < 65) {
-            futureCode = futureCode - 65 + 90 + 1
+            futureCode = futureCode - 65 + 90 + 1;
         }
-        return futureCode
+        return futureCode;
     }
     if (currentCode >= 97 && currentCode <= 122) {
         if (futureCode > 122) {
-            futureCode = futureCode - 122 + 97 - 1
+            futureCode = futureCode - 122 + 97 - 1;
         }
         if (futureCode < 97) {
-            futureCode = futureCode - 97 + 122 + 1
+            futureCode = futureCode - 97 + 122 + 1;
         }
-        return futureCode
+        return futureCode;
     } 
-    return currentCode
+    return currentCode;
 }
 
-console.log(encode(7, 'This is secret. Message about "_" symbol!'))
+// console.log(encode(-33, 'This is secret. Message about "_" symbol!'))
 // Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!
 //This is secret. Message about "_" symbol!
