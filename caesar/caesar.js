733de6e5fff504dfa541edf52cae107d31397810
IrinaKorbut <irisharomka96@gmail.com>
2021-05-08 14:15:08 +0200

    feat: implement input/output in terminal

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -4,24 +4,29 @@ const { pipeline } = require('stream')
 
 const validationArguments = require('./validation-arguments')
 const args = validationArguments()
-// console.log(args)
+console.log(args)
 
 const CaesarTransform = require('./transformStream');
 
-const inputFilePath = path.resolve(__dirname, args.input)
-const outputFilePath = path.resolve(__dirname, args.output)
+const inputFilePath = args.input ? path.resolve(__dirname, args.input) : args.input
+const outputFilePath = args.output ? path.resolve(__dirname, args.output) : args.output
 
 let existenceError = '';
-try {
-    fs.accessSync(inputFilePath, fs.constants.F_OK);
-} catch (err) {
-    existenceError += args.input
+
+if (inputFilePath) {
+    try {
+        fs.accessSync(inputFilePath, fs.constants.F_OK);
+    } catch (err) {
+        existenceError += args.input
+    }
 }
 
-try {
-    fs.accessSync(outputFilePath, fs.constants.F_OK);
-} catch (err) {
-    existenceError += existenceError ? `, ${args.output}` : args.output
+if (outputFilePath) {
+    try {
+        fs.accessSync(outputFilePath, fs.constants.F_OK);
+    } catch (err) {
+        existenceError += existenceError ? `, ${args.output}` : args.output
+    }
 }
 
 if (existenceError) {
@@ -30,9 +35,9 @@ if (existenceError) {
     process.exit(1)
 }
 
-const fileRead = fs.createReadStream(inputFilePath)
+const fileRead = inputFilePath ? fs.createReadStream(inputFilePath) : process.stdin
 const caesarTransform = new CaesarTransform();
-const fileWriter = fs.createWriteStream(outputFilePath, { flags: 'a' })
+const fileWriter = outputFilePath ? fs.createWriteStream(outputFilePath, { flags: 'a' }) : process.stdout
 
 fileWriter.on('finish', () => {
     process.exit(0);
