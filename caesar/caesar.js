557847a7dcf491d55949cd6379a86b1ec6d8c5a9
IrinaKorbut <irisharomka96@gmail.com>
2021-05-08 13:55:16 +0200

    feat: check existense files

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -4,12 +4,31 @@ const { pipeline } = require('stream')
 
 const validationArguments = require('./validation-arguments')
 const args = validationArguments()
-console.log(args)
+// console.log(args)
 
 const CaesarTransform = require('./transformStream');
 
 const inputFilePath = path.resolve(__dirname, args.input)
 const outputFilePath = path.resolve(__dirname, args.output)
+
+let existenceError = '';
+try {
+    fs.accessSync(inputFilePath, fs.constants.F_OK);
+} catch (err) {
+    existenceError += args.input
+}
+
+try {
+    fs.accessSync(outputFilePath, fs.constants.F_OK);
+} catch (err) {
+    existenceError += existenceError ? `, ${args.output}` : args.output
+}
+
+if (existenceError) {
+    existenceError = `No such file in directory (${existenceError}). Please enter another filepath/filename.`
+    process.stderr.write(existenceError)
+    process.exit(1)
+}
 
 const fileRead = fs.createReadStream(inputFilePath)
 const caesarTransform = new CaesarTransform();
@@ -25,7 +44,7 @@ pipeline(
     fileWriter,
     (error) => {
         if (error) {
-            process.stderr.write(error)
+            process.stderr.write('Something go wrong. Please, try again =)\n Error: ', error)
             process.exit(1)
         }
     }
